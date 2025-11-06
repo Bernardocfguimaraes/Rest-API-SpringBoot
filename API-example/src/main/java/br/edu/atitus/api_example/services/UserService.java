3c04d0dc9301ef8e3c96f55cd76f92cfb3a29719
Bernardo Guimarães <bernardocfguima@gmail.com>
2025-11-05 21:55:58 -0300

    Entidades, DTO, Controller and Servie

---
+++ API-example/src/main/java/br/edu/atitus/api_example/services/UserService.java
@@ -0,0 +1,34 @@
+package br.edu.atitus.api_example.services;
+
+import br.edu.atitus.api_example.entities.UserEntity;
+
+public class UserService {
+
+	public UserEntity save(UserEntity user) throws Exception {
+		if (user == null)
+			throw new Exception("Objeto Nulo");
+		if (user.getName() == null || user.getName().isEmpty())
+			throw new Exception("Nome Inválido");
+		user.setName(user.getName().trim());
+		
+		if (user.getEmail() == null || user.getEmail().isEmpty())
+			throw new Exception("E-mail Inválido");
+		user.setEmail(user.getEmail().trim());
+		
+		if (user.getPassword() == null || user.getPassword().isEmpty() || user.getPassword().length() < 8)
+			throw new Exception("Password Inválido");
+		
+		//TODO criar hash de senha
+		
+		//TODO validar permissão
+		
+		
+		
+		
+	
+		
+		//TODO enviar para a camada repository
+		return user;
+	}
+	
+}
