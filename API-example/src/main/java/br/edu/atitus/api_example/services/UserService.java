eb76f35d33afc447fd059bc3cd78a757ce11aa60
Bernardo Guimarães <bernardocfguima@gmail.com>
2025-11-11 23:34:24 -0300

    Configurando Spring Security

--- API-example/src/main/java/br/edu/atitus/api_example/services/UserService.java
+++ API-example/src/main/java/br/edu/atitus/api_example/services/UserService.java
@@ -1,8 +1,23 @@
 package br.edu.atitus.api_example.services;
 
-import br.edu.atitus.api_example.entities.UserEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import br.edu.atitus.api_example.entities.UserEntity;
+import br.edu.atitus.api_example.repositories.UserRepository;
+
+@Service
 public class UserService {
+	
+	private final UserRepository repository;
+	private final PasswordEncoder encoder;
+	
+
+	public UserService(UserRepository repository, PasswordEncoder encoder) {
+		super();
+		this.repository = repository;
+		this.encoder = encoder;
+	}
 
 	public UserEntity save(UserEntity user) throws Exception {
 		if (user == null)
@@ -18,16 +33,15 @@ public class UserService {
 		if (user.getPassword() == null || user.getPassword().isEmpty() || user.getPassword().length() < 8)
 			throw new Exception("Password Inválido");
 		
-		//TODO criar hash de senha
+		user.setPassword(encoder.encode(user.getPassword()));
 		
-		//TODO validar permissão
+		if(repository.existsByEmail(user.getEmail()))
+			throw new Exception("Já existe usuario cadastrado com este e-mail");
 		
 		
 		
+		repository.save(user);
 		
-	
-		
-		//TODO enviar para a camada repository
 		return user;
 	}
 	
